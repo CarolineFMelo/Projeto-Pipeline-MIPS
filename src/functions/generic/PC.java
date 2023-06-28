b9f10b5f40d5184b7f5d3ec96a3dafc56a99e3ca
Caroline Melo <84986038+CarolineFMelo@users.noreply.github.com>
2023-06-28 13:32:32 -0300

    adicionando clock

--- src/functions/generic/PC.java
+++ src/functions/generic/PC.java
@@ -3,6 +3,15 @@ package functions.generic;
 public class PC {
 	
 	static Integer pc = 0;
+	static Integer clock = 0;
+
+	public static Integer getClock() {
+		return clock;
+	}
+
+	public static void setClock(Integer clock) {
+		PC.clock = clock;
+	}
 
 	public static Integer getPc() {
 		return pc;
--- src/functions/stages/IF.java
+++ src/functions/stages/IF.java
@@ -6,9 +6,10 @@ public class IF {
 	
 	public String[] readingInstruction(String line) {
 		PC.setPc(PC.getPc()+1);
+		PC.setClock(PC.getClock()+1);
 		String[] op = line.split("\\s");
 		Integer aux = PC.getPc()-1;
-		System.out.println("\nPC: " + aux);
+		System.out.println("\nPC: " + aux + "\nClock: " + PC.getClock());
 		return op;
 	}
 	
